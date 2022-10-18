929281879ce40eb31c0ebb834df202b8e6e25766
URK21CO3019 <110014984+URK21CO3019@users.noreply.github.com>
2022-10-18 10:31:47 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,44 @@
 from typing import List
 
-def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+def merge_sort(data) -> None:
   # Write code here
+  if len(data) >1:
+    mid = (len(data))//2
+    data_left = data[:mid]
+    data_right = data[mid:]
+    merge_sort(data_left)
+    merge_sort(data_right)
+ 
+    a = 0 # for left_ata
+    b = 0 # for right_data
+    c = 0 # for main_data
 
+    while a<len(data_left) and b<len(data_right):
+      if data_left[a] <= data_right[b]:
+        data[c] = data_left[a]
+        a+=1
+      else:
+        data[c] = data_right[b]
+        b+=1
+      c+=1
+
+    while a < len(data_left):
+      data[c] = data_left[a]
+      a+=1
+      c+=1
+    while b < len(data_right):
+      data[c] = data_right[b]
+      b+=1
+      c+=1
+    return data
 
 # Do not change the following code
-nums1 = []
-nums2 = []
-for item in input().split(', '):
-  nums1.append(int(item))
-for item in input().split(', '):
-  nums2.append(int(item))
-m = int(input())
-n = int(input())
-merge(nums1, m, nums2, n)
-print(nums1)
+input_data = input()
+data = []
+for item in input_data.split(', '):
+  if item.isnumeric():
+    data.append(int(item))
+  elif item.lstrip("-").isnumeric():
+    data.append(int(item))
+merge_sort(data)
+print(data)
